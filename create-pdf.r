f1f41ba3454ded072f811b3e97dfcd7c7525df2e
Johan Ingvast <johan.ingvast@bioservo.com>
2017-01-27 23:52:06 +0100

    Now seem to work

--- create-pdf.r
+++ create-pdf.r
@@ -1,6 +1,9 @@
 %PDF-1.6
 REBOL []
 
+do to-rebol-file rejoin [ get-env("BIOSERVO") %/Tools/rebol/libs/printf.r ]
+
+Z: func [ msg con][ print rejoin [ msg ": " mold con ] con]
 pdf-bindings: make object! [
     ; Streams
     q: 'q   
@@ -17,70 +20,28 @@ pdf-bindings: make object! [
     dict: 'dict
 ]
 
-stream: 'stream
-endstream: 'endstream
+
+xrefs: copy []
 
 ;1 0 obj
 ;<</Type /Catalog /Pages 2 0 R>>
 ;endobj
-def: [
-  'catalog [
-	dict [
-	    /Type 'Catalog
-	    /Pages Xs pages 
-	]
-    ]
-;2 0 obj
-;<</Type /Pages /Kids [3 0 R 4 0 R] /Count 2>>
-;endobj
-    'pages [
-	dict [
-	    /Type 'Pages
-	    /Kids [ Xs mbox Xs cont ]
-	    /Count 2
-	]
-    ]
-;3 0 obj
-;<</MediaBox [0 0 800 500]>>
-;endobj
-    'mbox [
-	dict [
-	    /MediaBox [0 0 800 500]
-	]
-    ]
-;4 0 obj
-;<</Type /Page
-;/Parent 2 0 R
-;/Contents 5 0 R >>
-;endobj
-    'cont [
-	'dict [
-	    /Length none
-	]
-	stream 
-	175 720 m 500 | 300 800 400 600 v 100 650 50 75 re h S 
-	endstream
-    ]
-]
 
-Xs: func [ 'arg ][
-    o: context [ 
-	name: arg
-	f: func [] [
-	    reduce [ (index? find d to-lit-word name) + 1 / 2 0 'R]
-	]
-    ]
-    get in o 'f
+Xs: func [ 'name ] [
+    reduce [ (index? find names name) 0 'R]
 ]
 
 do-functions: func [ blk /local rslt ret ][
     rslt: copy []
+    ;Z "call to do-cuntion with" mold blk
     while [ not empty? blk ] [
-	if all [ word? first blk any-function? get first blk ]
+	if all [ word? first blk value? first blk any-function? get first blk ]
 	[
-	    change blk get first blk ]
+	    change blk get first blk
+	]
 	either any-function? first blk
 	[
+	    ;Z "FUnction" blk
 	    ret: do/next blk
 	    unless unset? first ret [
 		append rslt first ret
@@ -89,6 +50,7 @@ do-functions: func [ blk /local rslt ret ][
 	][
 	    either block? first blk
 	    [
+		;Z "Calling with" first blk
 		append/only rslt do-functions first+ blk
 	    ][
 		append rslt first+ blk
@@ -111,11 +73,10 @@ convert-strings: func [ blk ][
     ]
 ]
 
-Z: func [ msg con][ print rejoin [ msg ": " mold con ] con]
 
 to-pdf-string: func [ blk /local str ] [
     str: copy ""
-    print [ type? blk copy/part _: mold blk any [find _ newline tail _ ]]
+    ;print [ type? blk copy/part _: mold blk any [find _ newline tail _ ]]
     forall blk [
 	if new-line? blk [ if all [ not empty? str #" " = last str ] [ clear back tail str ] append str newline ]
 	case [
@@ -123,13 +84,12 @@ to-pdf-string: func [ blk /local str ] [
 		append str rejoin [ "[ " to-pdf-string first blk " ]" ]
 	    ]
 	    'dict = first blk [
-		print "FOund dict"
 		unless block? second blk [ make error! "Dict must be following dict keyword" ]
-		append str rejoin [ "<< " to-pdf-string Z "dict" second blk " >>" ]
+		append str rejoin [ "<< " to-pdf-string second blk " >>" ]
 		blk: next blk
 	    ]
 	    string? first blk [
-		append str first blk
+		append str rejoin [ "(" first blk ")" ]
 		append str " "
 	    ]
 	    true [
@@ -142,95 +102,112 @@ to-pdf-string: func [ blk /local str ] [
     str
 ]
 
-
-bind def pdf-bindings
-probe d: do-functions def
-probe dd: do-functions d
-convert-strings dd
-probe dd
-probe to-pdf-string dd
-halt
-
-
-
-d: copy []
-; make objects
-foreach [name obj] def [ repend d [name reduce bind obj pdf-bindings ] ]
-
-pdf-rslt: copy
-{%PDF-1.6
-}
-
-emit-pdf: func [ blk /as-is ][
-    either as-is
-    [ append pdf-rslt blk  ]
-    [
-	case  [
-	    any [ word? :blk  number? :blk ]
-		[ append pdf-rslt rejoin [ " " blk ] ]
-	    char? blk [ append pdf-rslt newline ]
-	    string? :blk 
-		[ repend pdf-rslt rejoin [ "(" blk ") " ] ]
-	    block? :blk
-		[ forall blk [ if new-line? blk [ emit-pdf/as-is newline ] emit-pdf first blk  ] ]
-	    refinement? :blk
-		[  append pdf-rslt rejoin [ mold :blk  " " ] ]
-	    any-function? :blk  
-		[ emit-pdf blk ]
-	    true [ print [ "Can't process " :blk ] ]
+names: copy []
+objs: copy []
+obj: func [ name blk /local obj ret ][
+    append names name
+    append objs context [
+	type: 'obj
+	name: last names
+	block: bind blk pdf-bindings
+	proc-func: func [][
+	    block: do-functions block
+	]
+	to-string: func [/local str ][
+	    str: reform [ index? find names name 0 "obj" newline ]
+	    append str to-pdf-string block
+	    repend str [ newline "endobj" ]
+	    str
 	]
     ]
+    last objs
 ]
 
-; Print out objects
-xrslt: copy []
-obj-names: copy []
-
-repeat obj-number (length? d) / 2 [
-
-    ; Put position into xref table
-    append xrslt length? pdf-rslt
-
-    emit-pdf reduce [ obj-number 0 'R 'obj newline ]
-
-    append obj-names first d
-
-    obj-content: second d
-
-    while [not empty? obj-content] [  ; Loop thru the vector
-	obj: first obj-content
-	case [
-	    obj = 'stream [
-		emit-pdf copy/part obj-content
-			      next obj-content: find obj-content 'endstream
-	    ]
-	    object? obj [
-		emit-pdf/as-is "<<"
-		foreach name next first obj [ 
-		    emit-pdf reduce [ to-refinement name  get in obj name ]
-		    ;if function? get in obj name [ dbg: get in obj name asdf]
-		    emit-pdf/as-is newline
-		]
-		emit-pdf/as-is reform [ ">>" newline ]
-	    ]
-	    true [ 
-		error reform ["Something not handled in" second d]
-	    ]
+stream: func [ name blk /local obj ret ][
+    append names name
+    append objs context [
+	type: 'stream
+	name: last names
+	block: bind blk pdf-bindings
+	dict: bind copy/part block
+	    stream: find block 'stream pdf-bindings
+	proc-func: func [][
+	    stream: do-functions stream
 	]
-	obj-content: next obj-content
+	to-string: func [/local str stream-str ] [
+	    str: reform [ index? find names name 0 "obj" newline ]
+	    stream-str: to-pdf-string stream
+	    change next find dict/dict /Length (length? stream-str)
+	    append str to-pdf-string dict
+	    append str newline
+	    append str stream-str
+	    repend str [ newline "endobj" ]
+	    str
+	]
     ]
-    d: skip d 2
+    last objs
 ]
+
+do def
+def: [
+  obj 'catalog [ dict
+	[ /Type /Catalog
+	    /Pages Xs pages 
+	] ]
+    obj 'pages [ dict [
+	    /Type /Pages
+	    /Kids [ Xs mbox Xs page ]
+	    /Count 2
+	]
+    ]
+    obj 'mbox [ dict [
+	    /MediaBox [0 0 800 500]
+	]]
+    obj 'info [ dict [
+	    /Creator "pdf-creator.r"
+	    /CreationDate to-string now
+    ] ]
+    obj 'page [ dict [ /Type /Page /Parent pages /Contents Xs cont] ]
+    stream 'cont [ dict [
+	    /Length none
+	]
+	stream 
+	175 720 m 500 | 300 800 400 600 v 100 650 50 75 re h S 
+	endstream
+    ]
+]
+str: "%PDF-1.6"
+forall objs [ 
+     objs/1/proc-func
+    append str newline
+    append xrefs length? str
+    append str objs/1/to-string
+]
+append str newline
+
+xref-str: copy ""
+
+append str reform [
+    "xref" newline
+    0 1 + length? objs newline
+    "0000000000 65535 f" newline
+    rejoin map-each x xrefs [ join sprintf [ "%010d 00000 n" x ]  newline ]
+    "trailer" newline
+    to-pdf-string do-functions [dict [
+	/Size add length? xrefs 1 
+	/Root Xs catalog
+	/Info Xs info
+    ]] newline
+    "startxref" newline
+    length? str newline
+    "%%EOF"
+]
+
+write %test.pdf str
     
-probe pdf-rslt
 
 
 
-;5 0 obj
-;<</Length 17>>
-;stream
-;endstream
-;endobj
 ;xref
 ;0 6
 ;0000000000 65535 f
