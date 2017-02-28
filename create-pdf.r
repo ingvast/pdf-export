acc626d99bf424b01a48b47cf5aadbb93e8cc6d0
Johan Ingvast <johan.ingvast@bioservo.com>
2017-02-28 08:22:43 +0100

    Cleaned and moved around

--- create-pdf.r
+++ create-pdf.r
@@ -76,7 +76,7 @@ Tw	set word spacing.
 Tz 	set horizontal scaling.
 T* 	move to start of next line.
 v 	curveto.
-w 	setlinewidth.
+w 	'setlinewidth.
 W 	clip.
 y 	curveto.
 }
@@ -147,7 +147,6 @@ eval-draw: func [
 ][
     reduce bind/copy blk rebol-draw-binding
 ]
-    
 
 xrefs: copy []
 
@@ -177,17 +176,6 @@ Xs: func [
     ][
 	make error! rejoin [ {Cannot find object '} name {'} ]
     ]
-]
-
-comment [
-refSort: func [
-    {Evaluate the argument block which should contain reference to objects.
-    Retrun the block sorted on the objects.}
-    blk [block!]
-][
-    change blk sort/skip probe do-functions blk 3
-    reduce [ blk ]
-]
 ]
 
 do-functions: func [
@@ -355,12 +343,11 @@ draw-circle: func [ x y rx /xy ry /local dx dy c m ][
 	x      y - ry 'c
 	x - dx y - ry
 	x - rx y - dy
-	x - rx y     'c
+	x - rx y     'c 'h
     ]
 ]
 
 rotating: func [
-
     {Rotates the angle alpha (degrees) around (x,y)
     A: 
 	[  ca sa 0 ]
@@ -374,9 +361,7 @@ rotating: func [
 	x0 = ca*x0-sa+tx
 	tx = x0 - ca*x0 + sa*y0
 	y0 = sa*x0+ca*y0+ty
-	ty = y0-sa*x0-ca*y0
-}
-
+	ty = y0-sa*x0-ca*y0}
     x0 y0 alpha
     /local ca sa Tx Ty cm Axx
 ][
@@ -444,11 +429,10 @@ face-to-page: func [
 
     fy-py: func [ y ][ current-face/size/y - y ]
     get-media-box: func [  ][
-	reduce [ reduce [ 0 0 current-face/size/x current-face/size/y  ] ]
+	reduce [ 0 0 current-face/size/x current-face/size/y  ]
     ]
 
     face-box: first get-media-box f
-
     
     contents: copy contents
     forall contents [ insert contents 'Xs first+ contents ]
@@ -456,7 +440,7 @@ face-to-page: func [
     obj name  compose/deep [ dict [ /Type /Page
 			/Parent Xs pages
 			/Contents [( contents) ]
-			/MediaBox get-media-box 
+			/MediaBox [(get-media-box )]
 			/Resources Xs (resources)
 		] ]
 
@@ -632,16 +616,18 @@ draw-to-stream: func [
     strea
 ]
 
-compose-file: func [ ]
-[
+compose-file: func [
+    /local str
+] [
     str: copy "%PDF-1.6"
-    forall objs [ 
-	print [ "Processing object" objs/1/name ]
-	objs/1/proc-func
+    foreach o objs [ 
+	print [ "Processing object" o/name ]
+	o/proc-func
 	append str newline
 	append xrefs length? str
-	append str objs/1/to-string
+	append str o/to-string
     ]
+
     append str newline
 
     xref-str: copy ""
@@ -661,6 +647,236 @@ compose-file: func [ ]
     ]
     str
 ]
+
+
+obj 'catalog [ dict
+    [ /Type /Catalog
+	/Pages Xs pages
+    ] ]
+
+obj 'pages [ dict [
+	/Type /Pages
+	/Kids [ Xs page ]
+	/Count 1
+    ]
+]
+
+obj 'info [ dict [
+	/Creator "pdf-creator.r"
+	/CreationDate to-string now
+] ]
+
+
+used-fonts: copy []
+font-translations: [
+    "helvetica"		Helvetica
+    "arial"		Helvetica
+    "font-sans-serif"	Helvetica
+    "FreeSans"		Helvetica
+    "Courier"		Courier
+    "font-fixed"	Courier
+    "Times"		Times-Roman
+    "Times-Roman"	Times-Roman
+    "font-serif"	Times-Roman
+]
+
+translate-fontname: func [ name [string! word! ] /local new ] [
+    if word? name [ name: to-string name ]
+    new: any [ select font-translations name to-word name ]
+    to-refinement new
+]
+
+use-font: func [ name /local use-name tmp ][
+    if object? name [
+	either all [ string? name/name find name/name "/" ][
+	    name: copy/part tmp: next find/last name/name "/" any [ find/last tmp "." tail tmp ]
+	][
+	    name: name/name
+	]
+    ]
+    append used-fonts use-name: translate-fontname name
+    use-name
+]
+    
+
+reduce-fonts: func [] [
+    used-fonts: union used-fonts []
+]
+
+create-fonts-resource: func [
+    /local dict
+][
+    dict: copy [
+    ]
+    foreach x used-fonts [
+	repend dict  [ x 'Xs to-word x ]
+	obj to-word x compose/deep [
+	    dict [
+		/Type /Font
+		/Subtype /Type1
+		/BaseFont   (to-refinement x )
+	    ]
+	]
+    ]
+    obj 'fonts-resource compose [ dict (reduce [ dict ] ) ]
+]
+
+{
+Exampe of how fonts should be organized
+obj page
+<<  /Type /Page
+    /Parent pages
+    /Contents [ conts ]
+    /MediaBox [ x y x y]
+    /Resources resources
+>>
+endobj
+obj conts
+<<>>
+endobj
+obj resources
+<<  /Font fonts
+    /Xref xrefs
+>>
+endobj
+obj fonts
+<<  /Helvetica helvetica
+    /Times-Roman times-roman
+>>
+endobj
+obj helvetica
+<<  /Type /Font
+    /Subtype /Type1
+    /BaseFont /Helvetica
+>>
+endobj
+obj times-roman
+<<  /Type /Font
+    /Subtype /Type1
+    /BaseFont /Times-Roman
+>>
+}
+    
+obj 'resources [
+    dict [
+	/Font Xs fonts-resource 
+    ]
+] 
+
+parse-face: func [
+    face [object!]
+    /local strea
+][
+    strea: copy []
+
+    fy-py: func [ y ][ face/size/y - y ]
+    
+    if face/color [ ; background
+	repend strea [
+	    face/color 'rg
+	    0 0 face/size 're 'f
+	]
+    ]
+    if face/image [
+    ]
+
+    if  block? p: face/effect [
+	offset: any [
+		    all [ face/edge face/edge/size ]
+		    0x0
+	]
+	while [p: find p 'draw] [
+	    append strea compose [
+		q
+		( translating offset/x offset/y )
+		(
+		    draw-to-stream/noregister/size
+			'anything p/2
+			face
+			face/size - ( 2x2 * offset )
+		)
+		Q    
+	    ]
+	    p: skip p  2
+	]
+    ]
+    if all [ face/text not find system/view/screen-face/pane face ] [
+	line-info: make system/view/line-info []
+	n: 0
+	while [  textinfo face line-info n ][
+	    edge: either all [ face/edge face/edge/size ][ face/edge/size ] [ 0x0 ]
+	    x: line-info/offset/x + edge/x
+	    y: face/size/y
+		- line-info/offset/y
+		- face/font/size
+		- edge/y
+	    append strea compose [
+		BT (use-font face/font/name) (face/font/size) Tf
+		(face/font/color) rg 
+		( reduce [ x y ] )
+		Td
+		(copy/part line-info/start line-info/num-chars) Tj
+		ET
+	    ]
+	    n: n + 1
+	]
+    ]
+    if all [ face/edge face/edge/size face/edge/color ][
+	use [ edge size nw-color se-color ][
+	    edge: face/edge/size size: face/size
+	    nw-color: se-color: face/edge/color
+	    switch face/edge/effect [
+		ibevel [
+		    nw-color: hsv-to-rgb (rgb-to-hsv face/edge/color) - 0.0.63
+		    se-color: hsv-to-rgb (rgb-to-hsv face/edge/color) + 0.0.63
+		]
+		bevel [
+		    nw-color: hsv-to-rgb (rgb-to-hsv face/edge/color) + 0.0.63
+		    se-color: hsv-to-rgb (rgb-to-hsv face/edge/color) - 0.0.63
+		]
+	    ]
+	    repend strea [
+		red 'RG
+		nw-color 'rg
+		0 0 'm
+		edge 'l
+		edge/x size/y - edge/y 'l
+		size/x - edge/x size/y - edge/y 'l
+		size 'l
+		0 size/y 'l
+		'h  'f
+		se-color 'rg
+		0 0 'm
+		edge 'l
+		size/x - edge/x edge/y 'l
+		size/x - edge/y size/y - edge/y 'l
+		size 'l
+		size/x 0 'l
+		'h 'f
+	    ]
+	]
+    ]
+    pane: get in face 'pane
+    if :pane [
+	print "*Parsing pane"
+	unless block? :pane [ pane: reduce [ :pane ] ]
+	foreach p pane [
+	    case [
+		object? :p [
+		    probe pos: as-pair p/offset/x face/size/y - ( p/offset/y + p/size/y)
+		    append strea 'q
+		    append strea translating pos/x pos/y
+		    append strea probe parse-face p
+		    append strea 'Q
+		]
+		function? :p [ print "Transformation of functional panes not implemented" ]
+		true	    [  print [ "Unknown object in pane!" type? :p ] ]
+	    ]
+	]
+    ]
+    strea
+]
+
 
     
 
