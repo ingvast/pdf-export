e1f7f40e8321659a16364948f9de1ea1334e6f40
Johan Ingvast <johan.ingvast@bioservo.com>
2017-05-03 08:56:19 +0200

    some more

--- printf.r
+++ printf.r
@@ -18,7 +18,7 @@ format: func [
 	val: fills: none
 
 	fillchar: #" "
-	align-char: #"+"
+	align-char: none
 	n-space: 0
 	n-fracts: 6
 	fmt-char: none
@@ -27,22 +27,23 @@ format: func [
 	digit: charset [ #"0" - #"9" ]
 
 	data-pattern: [
-	    ( align-char: #"+" fillchar: #" " )
+	    ( align-char: fillchar: signchar: none )
 	    #"%"
 	    opt [
 		[ copy align-char #"-" ]
 		|
 		[ copy fillchar #"0"   ]
+		| copy signchar #"+"
 	    ]
 	    copy n-space any digit
 	    opt [
-		#"."
-		    copy n-fracts any digit
+		#"." copy n-fracts any digit
 		]
 	    copy fmt-char [
 		#"f"
 		| #"g"
 		| #"e" 
+		| #"E" 
 		| #"d" 
 		| #"i" 
 		| #"s"
@@ -52,10 +53,15 @@ format: func [
 		n-fracts: to-integer any [ n-fracts 6 ]
 
 		val: first+ data
+		unless val [
+		    make error! "Not enough data to print, number of specifiers larger than data"
+		]
 
-		val: probe switch fmt-char [
-		    "s" [  to-string val ]
-		    "f" [ num-to-string-f val n-fracts ]
+		val: switch fmt-char [
+		    "s"		[ to-string val ]
+		    "d" "i"	[ num-to-string-d abs val ]
+		    "f"		[ num-to-string-f abs val n-fracts ]
+		    "e" "E"	[ num-to-string-e abs val n-fracts fmt-char  ]
 		]
 
 		fills: max 0 n-space - length? val
@@ -68,8 +74,27 @@ format: func [
 		]
 	    )
 	]
+
 	pattern: [
 	    any [ data-pattern | copy char skip ( append str char ) ] 
+	]
+
+	num-to-string-d: func [ num [ integer! ] /local str ] [
+	    str: copy ""
+
+	    unless num >= 0 [
+		append str "-"
+		str: next str
+		num: negate num
+	    ]
+	    until [
+		frac: mod num 10
+		insert str  round/floor frac
+		num: num - frac
+		num: num / 10
+		num < 1.0
+	    ]
+	    head str
 	]
 
 	num-to-string-f: func [ num n-fracts /local str ][
@@ -98,35 +123,60 @@ format: func [
 	    ]
 	    head str
 	]
+	num-to-string-e: func [
+	    num n-fracts modifier
+	    /local str  n
+	] [
+	    str: copy ""
+	    n: round/floor log-10 abs num
+	    num: num / (10.0 ** n )
+	    str: num-to-string-f num  n-fracts
+	    append str modifier
+	    append str n-fracts
+	]
+	    
+
+
+	parse/all/case fmt pattern 
     ]
-
-
-    probe  parse/all/case fmt o/pattern 
-    probe o
 
     return o/str
 ]
 	
 test-base: func [ fmt dta] [
-    print my: format fmt dta
-    print std: sprintf join [ fmt ] dta
+    prin my: format fmt dta
+    prin std: sprintf join [ fmt ] dta
     either my == std [
-	print "OK"
+	print "-- OK"
     ][
-	print "Fault"
+	print "--  Fault"
     ]
 ]
 test-string: does [
-    fmt: "<%s> <%10s>  <%-10s>  <%010s>"
+    fmt: "<%s> <%10s>  <%-10s>  <%010s> ---"
     dta: ["a" "b" "c" "d"]
     test-base fmt dta
 ]
 
 test-f: does [
-    fmt: "%f %10f %-10f %4.2f %-10.3f "
-    dta: reduce [ pi pi pi pi pi ]
+    fmt: "%f %10f %-10f %4.2f %-10.3f %3.8f ---"
+    dta: reduce [ pi pi pi pi pi  pi]
+    test-base fmt dta
+]
+test-e: does [
+    fmt: "%E %10e %-10E %4.2E %-10.3e %3.8E ---"
+    dta: reduce [ pi pi pi pi pi  pi]
+    test-base fmt dta
+]
+
+test-d: does [
+    fmt: "%d  %10d  %-10d %2d^/"
+    dta: reduce [ random 1000 random 1000 random 10000  3 ]
     test-base fmt dta
 ]
 
 test-string
 test-f
+test-e
+test-d
+
